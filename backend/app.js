d835ab56da13c72a221ecaea4b83565b92311d3d
vimansha <vimanshamadushani88@gmail.com>
2024-06-11 22:21:19 +0530

    feat: express app

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// midlewares
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+module.exports = app;
