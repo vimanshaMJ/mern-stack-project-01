388bd84852aec60b0ef6d7a98db4d0b4196e3763
vimansha <vimanshamadushani88@gmail.com>
2024-06-11 11:59:27 +0530

    feat: navigate to users page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from "./logo.svg";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome</h1>
-        <button className="user-button">Users</button>
+        <button className="user-button" onClick={() => navigate("/users")}>
+          Users
+        </button>
       </header>
     </div>
   );
