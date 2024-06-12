61006c5ce306ebb11d278f1dd7d13a6c6eef6f0e
vimansha <vimanshamadushani88@gmail.com>
2024-06-12 15:14:47 +0530

    feat: write CRUD services

--- backend/controller.js
+++ backend/controller.js
@@ -1,17 +1,55 @@
-const users = [
-  { id: 1, name: "Vimansha" },
-  { id: 2, name: "Gihan" },
-];
+const User = require("./model");
 
-// controller funtions to return above data , (cb - callback function)
-const getUsers = (cb) => {
-  cb(users);
+const getUsers = (req, res, next) => {
+  User.find() // find is equal to SELECT on sql, return a promise. We can access to a response & error by a promise
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
 };
 
-const getUsersById = (id, cb) => {
-  const user = users.find((user) => user.id == id);
-  cb(user);
+const addUser = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name,
+  });
+  user
+    .save()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
+};
+
+const updateUser = (req, res, next) => {
+  // const id = req.body.id;
+  // const name = req.body.name;  both are in one line:
+  const { id, name } = req.body;
+  User.updateOne({ id: id }, { $set: { name: name } })
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
+};
+
+const deleteUser = (rew, res, next) => {
+  const id = req.body.id;
+  User.deleteOne({ id: id })
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
 };
 
 exports.getUsers = getUsers;
-exports.getUsersById = getUsersById;
+exports.addUser = addUser;
+exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;
